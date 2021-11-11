690500816b2fce5f021f23852141831fdb68bd11
jw <jaewongogo@naver.com>
2021-11-11 19:23:28 +0900

    js files added

--- lottoGen2.js
+++ lottoGen2.js
@@ -5,14 +5,20 @@ const arr = []
 for(let i = 1; i<= 45; i++){
     arr.push(i)
 }
+
+console.log(arr)
 // 배열 출력
 console.log(arr)
-
+const arr2 = []
 for (let i = 0; i < 6; i++){
 //    const idx = 9
     const idx = parseInt(Math.random() * arr.length)
+    arr2.push(arr[idx]) // 값 출력
     console.log(arr.splice(idx,1))
+
 //    console.log(arr)
 }
+console.log(arr2)
+
 
 //
